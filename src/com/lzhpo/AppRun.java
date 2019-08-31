9b32de1c22cb316d2edbec30f5e9dab40c20ad7f
lzhpo <lzhpo@qq.com>
2019-08-31 12:51:22 +0800

    学习雪花算法之Java实现

---
+++ src/com/lzhpo/AppRun.java
@@ -0,0 +1,20 @@
+package com.lzhpo;
+
+/**
+ * <p> Author：lzhpo </p>
+ * <p> Title：测试雪花算法</p>
+ * <p> Description：
+ *
+ * 测试结果：生成的ID都是递增的，而且都是唯一的。
+ *
+ * </p>
+ */
+public class AppRun {
+    public static void main(String[] args) {
+        SnowFlake snowFlake = new SnowFlake(2, 3);
+        //循环生成2^10个ID（1024）
+        for (int i = 0; i < (1 << 10); i++) {
+            System.out.println(snowFlake.nextId());
+        }
+    }
+}
